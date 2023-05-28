e8636573006e8ea8886ad1a508350301936df60a
sushi-chaaaan <mail@sushichan.live>
2023-05-28 16:24:26 +0900

    Add validator

--- utils/validator.py
+++ utils/validator.py
@@ -3,8 +3,7 @@ from typing import Any, TypeVar
 T = TypeVar("T", bound=Any)
 
 
-def validate(var: Any, expected_type: type):
-    if not isinstance(var, type(expected_type)):
-        raise TypeError(f"{var} is not a {expected_type}")
-
-    pass
+def validate(var: Any, expected_type: type[T]) -> T:
+    if isinstance(var, expected_type):
+        return var
+    raise TypeError(f"{var} is not a {expected_type}")
