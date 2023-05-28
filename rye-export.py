cd57d4dd8305d8863af4364d0b54ca8de331b556
sushi-chaaaan <mail@sushichan.live>
2023-05-28 17:12:03 +0900

    🔨 Migrate into rye from poetry

---
+++ rye-export.py
@@ -0,0 +1,12 @@
+def main():
+    requirements_txt: list[str]
+    with open("requirements.lock") as f:
+        lock = f.readlines()
+        requirements_txt = [line for line in lock if not line.startswith("-e")]
+
+    with open("requirements.txt", "w") as fr:
+        fr.writelines(requirements_txt)
+
+
+if __name__ == "__main__":
+    main()
