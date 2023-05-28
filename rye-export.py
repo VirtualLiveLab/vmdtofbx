178b0d66c4464e023dc96cc582a19c6d631e20cd
sushi-chaaaan <mail@sushichan.live>
2023-05-28 17:28:48 +0900

    🎨 use const

--- rye-export.py
+++ rye-export.py
@@ -1,10 +1,14 @@
+REQUIREMENTS_TXT_PATH = "requirements.txt"
+REQUIREMENTS_LOCK_PATH = "requirements.lock"
+
+
 def main():
     requirements_txt: list[str]
-    with open("requirements.lock") as f:
+    with open(REQUIREMENTS_LOCK_PATH) as f:
         lock = f.readlines()
         requirements_txt = [line for line in lock if not (line.startswith("-e") or line.startswith("#") or line == "\n")]
 
-    with open("requirements.txt", "w") as fr:
+    with open(REQUIREMENTS_TXT_PATH, "w") as fr:
         fr.writelines(requirements_txt)
 
 
