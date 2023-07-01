603e05ecae1db85b34223126241ae5ef046137bc
sushi-chaaaan <mail@sushichan.live>
2023-07-02 00:49:26 +0900

    ✨ add /vote command

--- app/core/chat/vote/cog.py
+++ app/core/chat/vote/cog.py
@@ -1,24 +1,29 @@
+import os
 from typing import TYPE_CHECKING
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-from utils.validator import validate
+from const.enums import Color
+from utils.io import read_json
 
 if TYPE_CHECKING:
     # import some original class
     from app.bot import Bot
-    from app.core.chat.components.view import vote as vote_view
 
     pass
 
 
 class Vote(commands.Cog):
+    __renamed_options = {f"option{i}": f"選択肢{i}" for i in range(1, 21)}
+
     def __init__(self, bot: "Bot"):
         self.bot = bot
 
-    @app_commands.command(name="vote", description="投票を作成するよ！(最大25択)")
+    @app_commands.command(name="vote", description="最大20択で投票を作成するよ！選択肢を省略するとYes/Noになるよ！")
+    @app_commands.guilds(int(os.environ["GUILD_ID"]))
+    @app_commands.rename(**(__renamed_options | {"question": "質問文"}))
     async def vote(
         self,
         interaction: discord.Interaction,
@@ -43,52 +48,59 @@ class Vote(commands.Cog):
         option18: str | None = None,
         option19: str | None = None,
         option20: str | None = None,
-        option21: str | None = None,
-        option22: str | None = None,
-        option23: str | None = None,
-        option24: str | None = None,
-        option25: str | None = None,
     ):
         await interaction.response.defer(ephemeral=False)
         if interaction.channel is None:
             return
 
-        opts = [
-            option1,
-            option2,
-            option3,
-            option4,
-            option5,
-            option6,
-            option7,
-            option8,
-            option9,
-            option10,
-            option11,
-            option12,
-            option13,
-            option14,
-            option15,
-            option16,
-            option17,
-            option18,
-            option19,
-            option20,
-            option21,
-            option22,
-            option23,
-            option24,
-            option25,
+        valid_opts = [
+            opt
+            for opt in [
+                option1,
+                option2,
+                option3,
+                option4,
+                option5,
+                option6,
+                option7,
+                option8,
+                option9,
+                option10,
+                option11,
+                option12,
+                option13,
+                option14,
+                option15,
+                option16,
+                option17,
+                option18,
+                option19,
+                option20,
+            ]
+            if opt is not None and opt != ""
         ]
 
-        channel = validate(interaction.channel, discord.TextChannel)
-        y_or_n = True if all([opt is None for opt in opts]) else False
+        emoji_dict = read_json(r"const/vote_emoji.json")
+        if valid_opts == []:
+            option = [
+                {"name": emoji_dict["0"], "value": "はい"},
+                {"name": emoji_dict["1"], "value": "いいえ"},
+            ]
+        else:
+            option = [{"name": emoji_dict[str(i)], "value": valid_opts[i]} for i in range(len(valid_opts))]
 
-        if y_or_n:
-            view = vote_view.YesNoVoteView()
-            await channel.send(view=view)
+        embed = discord.Embed(
+            color=Color.MIKU,
+            title=question,
+        )
+        embed.set_author(name="投票")
+        for opt in option:
+            embed.add_field(**opt)
 
-        pass
+        msg = await interaction.followup.send(embed=embed, wait=True)
+        for e in [d["name"] for d in option]:
+            await msg.add_reaction(e)
+        return
 
 
 async def setup(bot: "Bot"):
