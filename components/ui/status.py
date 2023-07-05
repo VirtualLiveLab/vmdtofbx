130dabb0e2901883f1e0b8ded1753a32fd630216
sushi-chaaaan <mail@sushichan.live>
2023-07-06 01:25:11 +0900

    📝 Add docs to StatusUI

--- components/ui/status.py
+++ components/ui/status.py
@@ -1,7 +1,6 @@
 import datetime
 from copy import deepcopy
 from enum import IntEnum, auto
-from typing import Any
 
 import discord
 from discord import Embed, Webhook
@@ -25,6 +24,19 @@ class Status(IntEnum):
 
 
 class StatusContext:
+    """
+    A context for displaying the status of something.
+
+    Attributes
+    ----
+    key: :class:`str`
+        The key to identify the context.
+    message: :class:`str`
+        The message to display. This will be appeared in the embed description.
+    status: :class:`Status`
+        The status of the context. Defaults to :class:`Status.INITIALIZED`.
+    """
+
     def __init__(self, *, key: str, message: str, initial_status: Status = Status.INITIALIZED) -> None:
         self.key = key
         self.message = message
@@ -55,6 +67,9 @@ class StatusContext:
         self.__status = value
 
     def to_string(self) -> str:
+        """
+        Returns the string representation of the context.
+        """
         match self.__status:
             case Status.INITIALIZED:
                 return f"{INITIALIZED_EMOJI} {self.__label}"
@@ -68,31 +83,83 @@ class StatusContext:
                 return f"{PANIC_EMOJI} [PANIC OCCURRED] {self.__label}"
 
     def to_initialized(self, *, message: str) -> None:
+        """
+        Set the context to initialized.
+
+        Args
+        ----
+        message (`:class:str`):
+            The message to display.
+        """
         self.message = message
         self.status = Status.INITIALIZED
 
     def to_in_progress(self, *, message: str) -> None:
+        """
+        Set the context to in progress.
+
+        Args
+        ----
+        message (`:class:str`):
+            The message to display.
+        """
         self.message = message
         self.status = Status.IN_PROGRESS
 
     def to_success(self, *, message: str) -> None:
+        """
+        Set the context to success.
+
+        Args
+        ----
+        message (`:class:str`):
+            The message to display.
+        """
         self.message = message
         self.status = Status.SUCCESS
 
     def to_failed(self, *, message: str) -> None:
+        """
+        Set the context to failed.
+
+        Args
+        ----
+        message (`:class:str`):
+            The message to display.
+        """
         self.message = message
         self.status = Status.FAILED
 
 
 class StatusUI:
+    """
+    An embed UI for displaying the status of contexts.
+    """
+
     def __init__(
         self,
         *,
         color: int = 0x000000,
-        title: Any | None = None,
-        url: Any | None = None,
+        title: str | None = None,
+        url: str | None = None,
         timestamp: datetime.datetime | None = None,
     ):
+        """
+        Initialize a status UI.
+
+        Args
+        ----
+        color (`:class:int` | `None`):
+            The colour code of the embed. Defaults to 0x000000.
+        title (`:class:str` | `None`):
+            The title of the embed. Defaults to None.
+        url (`:class:str` | `None`):
+            The URL of the embed. This will be set as title url. Defaults to None.
+        timestamp (`:class:datetime.datetime` | `None`):
+            The timestamp of the embed content. This is an aware datetime.
+            If a naive datetime is passed, it is converted to an aware
+            datetime with the local timezone. Defaults to None.
+        """
         self.__contexts: dict[str, StatusContext] = {}
         self.embed_dict = Embed(
             color=color,
@@ -105,6 +172,14 @@ class StatusUI:
 
     @property
     def embed_dict(self) -> EmbedData:
+        """
+        Returns a deepcopy of `Embed.to_dict()`.
+
+        Returns
+        -------
+        EmbedData:
+            `Embed.to_dict()` of the embed.
+        """
         return deepcopy(self.__embed_dict)
 
     @embed_dict.setter
@@ -113,34 +188,114 @@ class StatusUI:
 
     @property
     def color(self) -> int | None:
+        """
+        Getter
+        ----
+        Returns the current color of the embed.
+
+        Returns
+        ----
+        `:class:int` | `None`:
+            The color code of the embed.
+        """
         return self.embed_dict.get("color", None)
 
     @color.setter
     def color(self, value: int) -> None:
+        """
+        Setter
+        ----
+        Set the color of the embed.
+
+        Args
+        ----
+        value (`:class:int`):
+            The color code to set.
+        """
         current = self.embed_dict
         current["color"] = value
         self.embed_dict = current
 
     def set_message(self, message: discord.Message) -> None:
+        """
+        Set message object to the UI.\\
+        This is required to update the UI.\\
+        If you use `StatusUI.send()`, this is automatically set.
+
+        Args
+        ----
+        message (`:class:discord.Message`):
+            The message object to set.
+        """
         self.__message = message
 
     def add(self, *, key: str, status: Status = Status.INITIALIZED, message: str) -> None:
+        """
+        Add a new status context to the UI.
+
+        Args
+        ----
+        key (`:class:str`):
+            Key of the context.
+        message (`:class:str`):
+            Message of the context. this will be displayed in the UI.
+        status (`:class:Status` | `None`):
+            Initial status of context. Defaults to Status.INITIALIZED.
+
+        Raises
+        ----
+        `:class:KeyError`:
+            If the `key` already exists.
+        """
         if key in self.__contexts.keys():
-            raise ValueError(f"Context {key} already exists")
+            raise KeyError(f"Context {key} already exists")
         self.__contexts[key] = StatusContext(key=key, message=message, initial_status=status)
         return
 
     def remove(self, *, key: str) -> None:
+        """
+        Remove a status context from the UI.
+
+        Args
+        ----
+        key (`:class:str`):
+            Key of the context you want to remove.
+
+        Raises
+        ----
+        `:class:KeyError`:
+            If the `key` does not exist.
+        """
         if key not in self.__contexts.keys():
-            raise ValueError(f"Context {key} does not exist")
+            raise KeyError(f"Context {key} does not exist")
         self.__contexts.pop(key)
         return
 
     def update(self, *, key: str, status: Status | None = None, message: str | None = None) -> None:
+        """
+        Update a status context in the UI.\\
+        You must provide at least one of `status` or `message`.
+
+        Args
+        ----
+        key (`:class:str`):
+            Key of the context you want to update.
+        status (`:class:Status` | `None`):
+            New status of the context. Defaults to None.
+        message (`:class:str` | `None`):
+            New message of the context. Defaults to None.
+
+        Raises
+        ----
+        `:class:ValueError`:
+            If both `status` and `message` are None.
+        `:class:KeyError`:
+            If the `key` does not exist.
+        """
         if status is None and message is None:
             raise ValueError("At least one of status or message must be provided")
         if key not in self.__contexts.keys():
-            raise ValueError(f"Context {key} does not exist")
+            raise KeyError(f"Context {key} does not exist")
 
         if status is not None:
             self.__contexts[key].status = status
@@ -148,16 +303,24 @@ class StatusUI:
             self.__contexts[key].message = message
         return
 
-    # def to_embed(self) -> Embed:
-    #     current = self.embed_dict
-    #     description = "\n".join(ctx.to_string() for ctx in self.contexts.values())
-    #     if len(description) > 4096:
-    #         raise ValueError("Description is too long")
-    #     current["description"] = description
-    #     return Embed.from_dict(current)
-
     @property
     def _embed(self) -> Embed:
+        """
+        Getter
+        ----
+        Returns the embed object of the UI.\\
+        Using this property is not recommended.\\
+        Useful if you want to edit the embed directly like `embed.add_field()`.
+
+        Raises
+        ----
+        `:class:ValueError`: If the description of embed is too long.
+
+        Returns
+        ----
+        `:class:discord.Embed`
+            The embed object of the UI.
+        """
         current = self.embed_dict
         description = "\n".join(ctx.to_string() for ctx in self.__contexts.values())
         if len(description) > 4096:
@@ -167,6 +330,16 @@ class StatusUI:
 
     @_embed.setter
     def _embed(self, value: Embed) -> None:
+        """
+        Setter
+        ----
+        Set the embed object of the UI.
+
+        Args
+        ----
+        value (`:class:discord.Embed`):
+            The embed object to set.
+        """
         self.embed_dict = value.to_dict()
 
     async def send(
@@ -174,9 +347,26 @@ class StatusUI:
         target: discord.abc.Messageable | discord.Interaction | Webhook,
         ephemeral: bool = False,
     ) -> None:
+        """
+        Send the UI to the target.
+
+        Args
+        ----
+        target (`:class:discord.abc.Messageable` |`:class:discord.Interaction` | `:class:discord.Webhook`):
+            The target to send the UI.
+        ephemeral: :class:`bool`
+            Indicates if the message should only be visible to the user who started the interaction.
+            If a view is sent with an ephemeral message and it has no timeout set then the timeout
+            is set to 15 minutes. Defaults to `False`.
+
+        Raises
+        ----
+        `:class:TimeoutError`:
+            If the `interaction.token` is expired.
+        """
         if isinstance(target, discord.Interaction):
             if target.is_expired():
-                raise ValueError("Interaction is expired")
+                raise TimeoutError("Interaction is expired")
             if target.response.is_done():
                 self.__message = await target.followup.send(embed=self._embed, ephemeral=ephemeral, wait=True)
                 return
@@ -190,6 +380,9 @@ class StatusUI:
             self.__message = await target.send(embed=self._embed)
 
     async def edit(self) -> None:
+        """
+        Edit the UI. This will fail if you did not set the message object to the UI.
+        """
         try:
             await self.__message.edit(embed=self._embed)
         except Exception as e:
