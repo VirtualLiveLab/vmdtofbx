7db43873f8b51d83db73090f203bfd77d82eadf2
sushi-chaaaan <mail@sushichan.live>
2023-08-22 17:38:00 +0900

    fix circular import

--- components/ui/view.py
+++ components/ui/view.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 import discord
 from pydantic import BaseModel, ConfigDict, Field
 
-from components.ui.state import State
+# from components.ui.state import State
 
 if TYPE_CHECKING:
     from components.ui.send import ViewSender
@@ -25,15 +25,15 @@ class View:
         loop: asyncio.AbstractEventLoop | None = None,
     ) -> None:
         self._loop = loop or asyncio.get_event_loop()
-        self.__states: dict[str, State] = {}
+        # self.__states: dict[str, State] = {}
         self.__sender: ViewSender | None = None
-        self.compose()
+        # self.compose()
 
-    def compose(self) -> None:
-        d = self.__dict__
-        for k, v in d.items():
-            if isinstance(v, State):
-                self.__states[k] = v
+    # def compose(self) -> None:
+    #     d = self.__dict__
+    #     for k, v in d.items():
+    #         if isinstance(v, State):
+    #             self.__states[k] = v
 
     def export(self) -> ViewObject:
         return ViewObject()
